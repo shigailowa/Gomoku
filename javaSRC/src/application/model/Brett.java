34ad9d6fa51953537396718b2ed091742d3a6047
joshinils <joshua.laskowski@web.de>
2017-12-19 06:30:44 +0100

    implemented grid

--- javaSRC/src/application/model/Brett.java
+++ javaSRC/src/application/model/Brett.java
@@ -1,30 +1,60 @@
 package application.model;
 
+import java.time.chrono.MinguoChronology;
 import java.util.ArrayList;
 import java.util.List;
+
+import javafx.scene.shape.Line;
 
 public class Brett {
 	
 	private int _dim; // Dimension des Spielbretts
-	private int _spieler;
+	private int _spieler; //anz spieler
 	
-	private List<SpielStein> _brett;
+	//array in dem die SPielsteine liegen
+	private SpielStein[][] _brett;
+	
+	private List<Line> _gitter;
+	
+	private double _gitterWeite;
 	
 	/*
 	 * Konstruiert ein leeres Brett mit dim Feldern im Quadrat
 	 * @param dim 
 	 */
-	public Brett(int dim)
-	{	if(dim <= 0)
+	public Brett(int dim, double x, double y)
+	{	
+		double min=Math.min(x, y);
+		double max=Math.max(x, y);
+		if(dim <= 0)
 			dim=1;
 		_dim=dim;
 		_spieler=2;
-		_brett=new ArrayList<SpielStein>(_dim*_dim);
+		_brett=new SpielStein[_dim][_dim];
+		_gitter=new ArrayList<Line>();
+		_gitterWeite=(int)Math.ceil(min/_dim);
+		for (int i = 0; i < _dim; i++)
+		{
+			_gitter.add(new Line(i*_gitterWeite+_gitterWeite/2,                   _gitterWeite/2,
+								 i*_gitterWeite+_gitterWeite/2, _gitterWeite*_dim-_gitterWeite/2)); // vertikale linien
+			_gitter.add(new Line(               _gitterWeite/2,    i*_gitterWeite+_gitterWeite/2,
+							  _gitterWeite*_dim-_gitterWeite/2,    i*_gitterWeite+_gitterWeite/2)); // horizonatale linien
+		}
 	}
 	
 	public SpielStein at(int x, int y)
-	{	if(x>=0&&y>=0)//liegt (x, y) auf brett?
-			return _brett.get(x+_dim*y);
+	{
+		if(x>=0&&y>=0)//liegt (x, y) auf brett?
+			return _brett[x][y];
 		return null;
 	}
+	
+	public int getDim() 
+	{	return _dim;	}
+	
+	public List<Line> getGitter()
+	{	return _gitter;	}
+	
+	public double getGitterWeite() 
+	{	return _gitterWeite;	}
 }
--- javaSRC/src/application/view/SpielController.java
+++ javaSRC/src/application/view/SpielController.java
@@ -2,14 +2,32 @@ package application.view;
 
 import application.model.Brett;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
 
 public class SpielController {
 	@FXML AnchorPane gameAnchorPane;
 	Brett spielbrett;
 	
+	//test
+	Circle c;
+	
 	@FXML
 	private void initialize()
-	{	spielbrett=new Brett(17);
+	{
+		spielbrett=new Brett(10, gameAnchorPane.getPrefWidth(), gameAnchorPane.getPrefHeight());
+		c=new Circle(-10, -10, spielbrett.getGitterWeite()/2-1);
+		gameAnchorPane.getChildren().add(c);
+		
+		gameAnchorPane.getChildren().addAll(spielbrett.getGitter());
+	}
+	
+	@FXML
+	private void handleMouseMoved(MouseEvent event)
+	{
+//		System.out.println(event.getX()+" "+event.getY());
+		c.setCenterX(event.getX()-event.getX()%spielbrett.getGitterWeite()+spielbrett.getGitterWeite()/2);
+		c.setCenterY(event.getY()-event.getY()%spielbrett.getGitterWeite()+spielbrett.getGitterWeite()/2);
 	}
 }
